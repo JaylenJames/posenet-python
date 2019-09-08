513dd99cbb616a5ee2ac22f9b7d64abec7e67edf
JaylenJames <31025434+JaylenJames@users.noreply.github.com>
2019-09-08 18:44:41 -0500

    Update plot_keypoints.py

--- plot_keypoints.py
+++ plot_keypoints.py
@@ -3,10 +3,24 @@
 Created on Sun Sep  1 18:10:18 2019
 
 @author: Jaylen James
+Code will plot the keypoint coordinates vs time in order to assign the maximum 
+    value from this plot to the real-world distance measurement. This will be
+    the label.
+    
+Meeting: 
+    
 """
+import pandas as pd
 
 #Edit data within file.
 
 #Open file and set to a certain variable
+df = pd.read_csv('thesavedones.csv', header=None)
+
+#Nose locations
+data = df.loc[::17,3]
+
+reorg = pd.DataFrame(data, columns = ['Nose'])
+
 
 # Generate plots of keypoint coordinate position vs time.
