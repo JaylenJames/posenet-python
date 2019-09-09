e50ed2c5db1aa98baae4488f5db8f5f18a62911c
JaylenJames <31025434+JaylenJames@users.noreply.github.com>
2019-09-08 21:05:20 -0500

    Update plot_keypoints.py

--- plot_keypoints.py
+++ plot_keypoints.py
@@ -20,11 +20,18 @@ df = pd.read_csv('thesavedones.csv', header=None)
 #Nose locations
 part_data_df= df.iloc[::17,3]
 
-part_data_split_df = part_data_df.str.split(" ",expand=True) #splits string into muliple columns
+#Remove brackets and spaces before string and after numbers.
+part_edit_df = part_data_df.map(lambda x: x.lstrip('[ ').rstrip(' ]'))
+
+#Replace spaces between numbers with a comma
+part_edit2_df = part_edit_df.map(lambda x: x.replace("   ",",").replace("  ",",").replace(" ",","))
+
+#splits string into muliple columns
+part_split_df = part_edit2_df.str.split(",",expand=True)
 
 
-
-#part_data_df = part_data_df.astype(int) #convert keypoint coordinates to integer values
+#part_data_ints_df = part_split_df["0"].astype(int) #convert keypoint coordinates to integer values
+#part_data_ints_df = pd.to_numeric(part_split_df)
 
 reorg = pd.DataFrame(part_data_df, columns = ['Nose'])
 
