141f3c47ce13e43242e97efb9b3dd04be99a809d
JaylenJames <31025434+JaylenJames@users.noreply.github.com>
2019-09-11 21:33:03 -0500

    Update plot_keypoints.py

--- plot_keypoints.py
+++ plot_keypoints.py
@@ -22,31 +22,47 @@ df = pd.read_csv('thesavedones.csv', header=None)
 part_data_df= df.iloc[::17,3]
 rankle_data_df = df.iloc[::13,3]
 
+data_sets = [part_data_df, rankle_data_df]
+
+locations = [] #Initializing a list in order to append df at end of loop
+
     #Create loop to perform this data manipulation for each data set, starting here:
-#Remove brackets and spaces before string and after numbers.
-part_edit_df = part_data_df.map(lambda x: x.lstrip('[ ').rstrip(' ]'))
+for i in range(len(data_sets)):
+    #Remove brackets and spaces before string and after numbers.
+    data_select = data_sets[i]
+    
+    part_edit_df = data_select.map(lambda x: x.lstrip('[ ').rstrip(' ]'))
+    #rankle_edit_df = rankle_data_df.map(lambda x: x.lstrip('[ ').rstrip(' ]'))
+    
+    #Replace spaces between numbers with a comma
+    part_edit2_df = part_edit_df.map(lambda x: x.replace("   ",",").replace("  ",",").replace(" ",","))
+    #rankle_edit2_df = rankle_edit_df.map(lambda x: x.replace("   ",",").replace("  ",",").replace(" ",","))
+    
+    #splits string into muliple columns
+    part_split_df = part_edit2_df.str.split(",",expand=True)
+    #rankle_split_df = 
+    
+    #convert keypoint coordinates to integer values
+    #part_data_ints_df = part_split_df["0"].astype(int) 
+    part_data_ints_col0_df = pd.to_numeric(part_split_df[0])
+    part_data_ints_col1_df = pd.to_numeric(part_split_df[1])
+    
+    #Concatenate columns
+    part_location_data = pd.concat([part_data_ints_col0_df, part_data_ints_col1_df], axis=1)
+    
+    locations.append(part_location_data) #saves the location data to the locations variable
+    
+    
 
-#Replace spaces between numbers with a comma
-part_edit2_df = part_edit_df.map(lambda x: x.replace("   ",",").replace("  ",",").replace(" ",","))
-
-#splits string into muliple columns
-part_split_df = part_edit2_df.str.split(",",expand=True)
-
-
-#convert keypoint coordinates to integer values
-#part_data_ints_df = part_split_df["0"].astype(int) 
-part_data_ints_col0_df = pd.to_numeric(part_split_df[0])
-part_data_ints_col1_df = pd.to_numeric(part_split_df[1])
-
-#Concatenate columns
-part_location_data = pd.concat([part_data_ints_col0_df, part_data_ints_col1_df], axis=1)
-    #And ending here.
-
-reorg = pd.DataFrame(part_location_data, columns = ['Nose-x', 'Nose-y'])
-
+#reorg = pd.DataFrame(part_location_data, columns = ['Nose-x', 'Nose-y'])
 
 # Generate plots of keypoint coordinate position vs time.
 
+plt.figure(1)
 plt.plot(part_location_data[0])
+plt.show()
+
+plt.figure(2)
+plt.plot(part_location_data[1], 'm')
 plt.show()
 
